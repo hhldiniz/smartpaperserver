82cf52589b3b3fb96e8bf309e902bc84c7ecfa7a
Hugo Diniz <hhldiniz@gmail.com>
2018-03-30 09:35:31 -0300

    armazenando texto do html em um array e especializando filtro de tag html

--- miners/ScienceDirectMiner.py
+++ miners/ScienceDirectMiner.py
@@ -39,5 +39,5 @@ class ScienceDirectMiner(Miner):
         con = requester.get_connection("GET")
         consumer = HTMLConsumer()
         consumer.feed(str(con.content))
-        # print(consumer.get_tag_content("div", ['class', 'ResultList']))
+        print(consumer.get_tag_content("div", ['class', '']))
         return con.content
--- utils/HTMLConsumer.py
+++ utils/HTMLConsumer.py
@@ -5,9 +5,7 @@ class HTMLConsumer(HTMLParser):
     def __init__(self):
         super().__init__()
         self.__tags_attrs = {}
-
-    def get_tags_attrs(self):
-        return self.__tags_attrs
+        self.__html_text = []
 
     def error(self, message):
         print(message)
@@ -15,21 +13,35 @@ class HTMLConsumer(HTMLParser):
     def handle_starttag(self, tag, attrs):
         try:
             value = self.__tags_attrs.__getitem__(tag)
+            attrs.append(('text', self.get_html_text()[self.get_html_text().__len__()-1]))
             value.append(attrs)
             self.__tags_attrs.__setitem__(tag, value)
         except KeyError:
             self.__tags_attrs.__setitem__(tag, [attrs])
 
     def handle_data(self, data):
-        print(data)
+        self.set_html_text(data)
 
     def unknown_decl(self, data):
         print(f"Conteúdo desconhecido: #{data}")
 
     def get_tag_content(self, tag_name, tag_attrs):
         try:
-            all_tags_attrs = self.__tags_attrs.__getitem__(tag_name)
+            if tag_name is not None and tag_name is not "":
+                all_tags_attrs = self.__tags_attrs.__getitem__(tag_name)
+            else:
+                all_tags_attrs = self.__tags_attrs
             for attr in all_tags_attrs:
                 print(attr)
+            return all_tags_attrs
         except KeyError:
             return None
+
+    def get_tags_attrs(self):
+        return self.__tags_attrs
+
+    def set_html_text(self, new_text):
+        self.__html_text.append(new_text)
+
+    def get_html_text(self):
+        return self.__html_text
