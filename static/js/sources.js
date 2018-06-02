c87084ec25dbf600d0349aa01b0791fadcabc5d3
Hugo Diniz <hhldiniz@gmail.com>
2018-06-01 21:59:15 -0300

    inicio de form para fontes de dados

--- static/js/sources.js
+++ static/js/sources.js
@@ -1,3 +1,5 @@
 $(document).ready(()=>{
-   // $("#sources_form")
+   $("#sources_form").append("<div class='col s8'><button class=\"btn waves-effect waves-light\" type=\"button\" id=\"submit_search\" name=\"action\">Submit\n" +
+       "<i class=\"material-icons right\">send</i>\n" +
+       "</button></div>");
 });
