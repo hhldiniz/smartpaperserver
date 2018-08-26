cf20ff6a9eb443d19c57d58f0d6896c15c471268
Hugo Diniz <hhldiniz@gmail.com>
2018-08-26 19:50:57 -0300

    adaptações para o minerador obter categorias e novos testes

--- tests/MinerTests.py
+++ tests/MinerTests.py
@@ -3,10 +3,25 @@ import unittest
 from utils.Miner import Miner
 
 
-class MiningTest(unittest.TestCase):
+class NumbersMiningTest(unittest.TestCase):
 
     def setUp(self):
         self.__miner = Miner(data=[5, 6, 7, 9, 5, 3, 1, 4, 7, 3, 9, 0])
+
+    def test_mine(self):
+        self.__miner.mine()
+
+
+class StringsMiningTest(unittest.TestCase):
+
+    def setUp(self):
+        self.__miner = Miner(data=["a", "c", "y", "d", "p", "b", "x", "g"],
+                             categories=[
+                                 'tecnologia',
+                                 'saúde',
+                                 'arte',
+                                 'música',
+                             ])
 
     def test_mine(self):
         self.__miner.mine()
--- utils/Miner.py
+++ utils/Miner.py
@@ -3,12 +3,16 @@ from sklearn.cluster import KMeans
 
 
 class Miner:
-    def __init__(self, n_groups=8, data=None):
+    def __init__(self, n_groups=8, data=None, dtype=object, categories=None):
+        if categories is None:
+            categories = []
         if data is None:
             data = []
         self.__n_groups = n_groups
-        self.__data = numpy.array(data)
+        self.__categories = categories
+        self.__data = numpy.array(data, dtype=dtype)
 
     def mine(self):
         kmeans = KMeans(self.__n_groups, random_state=170)
+        print(self.__data)
         return kmeans.fit(self.__data.reshape(-1, 1))
