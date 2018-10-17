0c96b08af7f69f2a1fee5929f5eccccf9d911947
Hugo Diniz <hhldiniz@gmail.com>
2018-10-17 20:42:05 -0300

    remoção da view de resultados e preparação de view index para tratar os resultados via javascript

--- smartpaperserver.py
+++ smartpaperserver.py
@@ -1,16 +1,17 @@
+import json
+
 from flask import Flask, session, request, send_from_directory
 
-from utils.DBController import DBController
-from models.User import User
 from models.Article import Article
+from models.User import User
 from modules.Login import Login
-from views.IndexView import IndexView
-from views.AboutView import AboutView
-from views.SignupView import SignupView
-from views.HistoryView import HistoryView
-from views.SourcesView import SourcesView
-import json
+from utils.DBController import DBController
 from utils.MakePDF import MakePDF
+from views.AboutView import AboutView
+from views.HistoryView import HistoryView
+from views.IndexView import IndexView
+from views.SignupView import SignupView
+from views.SourcesView import SourcesView
 
 app = Flask(__name__)
 app.secret_key = "E7162800D84BFB861148F6F8E17462697866C542FE2E0E7D87AF0D01E209AB12"
@@ -85,11 +86,12 @@ def download_history():
         return json.dumps({"result": False})
 
 
-index_view = IndexView(template_name="index.html")
+index_view = IndexView("index.html")
 about_view = AboutView("about.html")
 signup_view = SignupView("signup.html")
 history_view = HistoryView("history.html")
 sources_view = SourcesView("sources.html")
+
 app.add_url_rule("/", methods=["GET", "POST"],
                  view_func=index_view.as_view("index", template_name=index_view.get_template_name()))
 app.add_url_rule("/about", view_func=about_view.as_view("about", template_name=about_view.get_template_name()))
--- static/js/index.js
+++ static/js/index.js
@@ -1,16 +1,29 @@
 $(document).ready(function () {
-    $(".modal").modal();
+    let resultCollection = $(".result_collection");
+    resultCollection.hide();
     $("#submit_search").click(()=> {
         $.ajax({
             url: "/",
             method: "POST",
             data: {"key": $("#key").val(), "hidden": $("#search_hidden").val()},
             success: data=>{
-                $("#search_result_text").text(data);
-                $("#search_result").modal("open");
+                data = JSON.parse(data);
+                for(let key in data)
+                {
+                    if(data.hasOwnProperty(key))
+                        for(let result in data[key])
+                        {
+                            if(data[key].hasOwnProperty(result))
+                            {
+                                result = JSON.parse(data[key]);
+                                $(".result_collection").append(`<li class='collection-item'>${data[key][result]}</li>`);
+                            }
+                        }
+                }
+                resultCollection.show();
             },
             error: err=>{
-
+                console.error(err)
             }
         });
     });
